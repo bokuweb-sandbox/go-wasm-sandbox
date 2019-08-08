d727773baff852ebe65df6541ccd556fb0c91db7
bokuweb <bokuweb12@gmail.com>
2019-08-09 02:08:13 +0900

    array sample

--- wasm_main.go
+++ wasm_main.go
@@ -8,6 +8,13 @@ import (
 )
 
 func newTest(this js.Value, args []js.Value) interface{} {
+	fmt.Println(args[0].Get("length"))
+
+	// fmt.Println(args[0][1])
+	for i := 0; i < args[0].Get("length").Int(); i++ {
+		fmt.Println(args[0].Index(i))
+	}
+
 	c := []byte{1}
 	this.Set("buf", js.TypedArrayOf(c))
 	this.Set("change", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
@@ -18,7 +25,7 @@ func newTest(this js.Value, args []js.Value) interface{} {
 }
 
 func main() {
-	fmt.Println("hello")
+	fmt.Println("hello!!!")
 	window := js.Global()
 	window.Set("Test", js.FuncOf(newTest))
 	select {}
